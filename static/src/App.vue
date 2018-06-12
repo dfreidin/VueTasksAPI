60a6ea93d1d71db1ccdd352e8c1c739b13120fbf
dfreidin <36039896+dfreidin@users.noreply.github.com>
2018-06-11 18:08:18 -0700

    added create form

--- static/src/App.vue
+++ static/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app" class="container">
+    <h1>Add Task:</h1>
+    <task-form v-on:handle-task="createTask" />
     <h1>All the tasks:</h1>
     <TaskList v-on:show-task="showTask" v-bind:tasks="tasks" />
     <ShowTask v-bind:task="show_task" />
@@ -8,14 +10,16 @@
 
 <script>
 const axios = require("axios");
-import TaskList from "./components/TaskList.vue"
-import ShowTask from "./components/ShowTask.vue"
+import TaskList from "./components/TaskList.vue";
+import ShowTask from "./components/ShowTask.vue";
+import TaskForm from "./components/TaskForm.vue";
 
 export default {
   name: 'app',
   components: {
     TaskList,
-    ShowTask
+    ShowTask,
+    TaskForm
   },
   data() {
       return {
@@ -37,6 +41,13 @@ export default {
       showTask(task) {
         axios.get(`/tasks/${task._id}`).then(response => {
           this.show_task = response.data.data;
+        }).catch(error => {
+          console.log(error);
+        });
+      },
+      createTask(task) {
+        axios.post("/tasks", task).then(response => {
+          this.getTasks();
         }).catch(error => {
           console.log(error);
         });
---
+++ static/src/components/TaskForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <form>
+        <p>Title: <input v-model="task.title" type="text"></p>
+        <p>Description: <input v-model="task.description" type="text"></p>
+        <p><button v-on:click="sendTask" class="btn btn-primary" type="button">Submit</button>
+    </form>
+</template>
+
+<script>
+export default {
+    props: {
+        inputTask: {
+            default: {
+                title: "",
+                description: ""
+            }
+        }
+    },
+    data() {
+        return {
+            task: {}
+        };
+    },
+    mounted() {
+        this.loadTask();
+    },
+    methods: {
+        loadTask() {
+            Object.assign(this.task, this.inputTask);
+        },
+        sendTask() {
+            this.$emit("handle-task", this.task);
+        }
+    }
+}
+</script>
